ed196d48b24534483af6c414966817d2e065575b
tuke307 <tonymeissner70@gmail.com>
2020-12-20 22:02:14 +0100

    fixes

--- RunAway!/Sample/ViewModels/ActivityHeaderPageViewModel.cs
+++ RunAway!/Sample/ViewModels/ActivityHeaderPageViewModel.cs
@@ -8,7 +8,8 @@ using Prism.Navigation;
 
 using Sample.Domain;
 using Sample.Localization;
-
+using Sharpnado.Presentation.Forms;
+using Sharpnado.Tasks;
 using SkiaSharpnado.Maps.Domain;
 using SkiaSharpnado.ViewModels;
 
@@ -27,25 +28,25 @@ namespace Sample.ViewModels
         {
             _activityService = activityService;
 
-            //Loader = new ViewModelLoader<List<ActivityHeaderViewModel>>(emptyStateMessage: AppResources.EmptyActivityMessage);
+            Loader = new TaskLoaderNotifier<List<ActivityHeaderViewModel>>(/*emptyStateMessage: AppResources.EmptyActivityMessage*/);
             ActivityTappedCommand = new Command<ActivityHeaderViewModel>(
                 item => NavigationService.NavigateAsync($"ActivityPage?activityId={item.Id}"));
         }
 
         public ICommand ActivityTappedCommand { get; }
 
-        //public ViewModelLoader<List<ActivityHeaderViewModel>> Loader { get; }
+        public TaskLoaderNotifier<List<ActivityHeaderViewModel>> Loader { get; }
 
         public override void OnNavigatedTo(NavigationParameters parameters)
         {
             Title = AppResources.ActivityHeaderPageTitle;
 
-            //if (Loader.IsSuccessfullyCompleted)
-            //{
-            //    return;
-            //}
+            if (Loader.IsSuccessfullyCompleted)
+            {
+                return;
+            }
 
-            //Loader.Load(LoadAsync);
+            Loader.Load(LoadAsync);
         }
 
         private async Task<List<ActivityHeaderViewModel>> LoadAsync()
